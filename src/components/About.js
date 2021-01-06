c659e00c9568325e05c2d18df4edccd33b1dd440
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-01-06 14:22:55 +0000

    typo fix

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,8 @@ export default class About extends Component {
             <img src={Poster} className="background"/>
             <div className="text-block">
                 <p>
-                    Here you can find all of our info
-                    You can also send in your project ideas for our latest campaign. Currently a fully repsonsive basic web site.
+                    Here you can find all of our info:
+                    You can also send in your project ideas for our latest campaign. Currently a fully responsive basic web site.
                     No routing done yet though.
                 </p>
             </div>
