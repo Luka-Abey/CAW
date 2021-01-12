1caa6cae91d8b8b04ab3d595c9ff29a14ffee429
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-01-12 17:24:44 +0000

    added simple submission schema

--- server/models/submission.js
+++ server/models/submission.js
@@ -1,1 +1,28 @@
-import mongoose from "mongoose";
+const express = require('express');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SubmissionSchema = new Schema({
+  name: {
+    type: String,
+    required: false
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  text: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Submission = mongoose.model('post', SubmissionSchema); 
