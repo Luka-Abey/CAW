79551e334cc9d389c91db7125a129a12720a5b07
Robert Sahakyan <r.sahakyan1@gmail.com>
2021-01-14 21:35:28 +0000

    stable home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,59 +1,37 @@
 import React from 'react';
 
-const Home: React.FC = () => (
-  
-  <div className="wallpaper-container">
-    <div className="welcome-content">
-      {/* <h1 className="title">Welcome To</h1> */}
-      <img className="img-fluid" src="https://imgur.com/fNMGwNr.png" alt="logo"/>
+const Home: React.FC = () => {
+ 
+  return (
+  <>
+    <div className="wallpaper-container">
+      <div className="welcome-content">
+        {/* <h1 className="title">Welcome To</h1>  */}
+        <img className="img-fluid" src="https://imgur.com/fNMGwNr.png" alt="logo"/>
+      </div>
+    </div> 
 
-    </div>
-  
+    {/* ======================================================================================== */}
     <div className="main-content-container">
       <div className="about-caw-info">
       <h2 className="custom-heading">Who are C.A.W</h2>
-
-        <p>Community Action on Waste is a Leeds based community co-operative , made up of local residents, alumni and students. Every year, in June, hundreds of tonnes of waste is left on the streets of Leeds. The kind of things left are exactly the kind of things new students will be buying new in September. We want this to end. 
-        You might know us from our Free Shops, which we ran back in September. We put a load of stuff donated by students in June into one of our members' basements. Then, we set up shop in September to give it back to students as they arrived in Leeds - pay as you feel. We raised over £250 and we’re very happy about this!
-        </p>
+      <p>Community Action on Waste is a Leeds based community co-operative , made up of local residents, alumni and students. Every year, in June, hundreds of tonnes of waste is left on the streets of Leeds. The kind of things left are exactly the kind of things new students will be buying new in September. We want this to end.
+      <br/>
+      You might know us from our Free Shops, which we ran back in September. We put a load of stuff donated by students in June into one of our members' basements. Then, we set up shop in September to give it back to students as they arrived in Leeds - pay as you feel. We raised over £250 and we’re very happy about this!
+      </p>
       </div>
       <div className="current-project-info">
         <h2 className="custom-heading">Current Project</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat rem aut architecto deleniti laboriosam tenetur consequuntur maxime, doloremque eum laborum harum fugit iste numquam ut esse enim magnam animi quam.</p>
       </div>
-
-    
-
+    </div>
+    {/* ======================================================================================== */}
     <footer>
       <p>Contact Us: <a href="https://www.facebook.com/CommunityActionWaste">Facebook</a></p>
     </footer>
-    
+  </>
+  )
+};
 
-   
-
-    </div>
-  </div>
-  // <body className="text-center" > 
-  //   <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
-  //     <Header /> 
-      
-  //     <main role="main" className="inner cover">
-  //       <img src="../media/logo.jpg" alt="logo" />
-  //       <h1 className="cover-heading">Community Action on Waste</h1>
-  //       <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laboriosam voluptatem autem eius nobis ab ex iure aliquam deserunt eligendi perferendis sunt, necessitatibus maxime reiciendis inventore error ratione excepturi quis..</p>
-
-  //       <h2 className="">About</h2>
-  //       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sed corrupti iure dolor? Quia voluptate id, eius temporibus autem distinctio perspiciatis ad quas minus magni assumenda fugit eos expedita omnis?</p>
-
-  //     </main>
-
-  //     <footer className="card-footer">
-  //       <div className="inner">
-  //         <p>Check out our social media <a href="https://www.facebook.com/CommunityActionWaste">Facebook </a>.</p>
-  //       </div>
-  //     </footer>
-  //   </div>
-  //   </body>
-);
 
 export default Home;
