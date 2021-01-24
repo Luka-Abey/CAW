5fe06c64d2ca1d424b804d9aee5b4fcbddd75177
MemorySounds <r.sahakyan1@gmail.com>
2021-01-24 23:24:44 +0000

    many changes

--- client/src/pages/Submission/Submission.tsx
+++ client/src/pages/Submission/Submission.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Footer from '../../components/Footer/Footer'
+import Wallpaper from '../../components/Wallpaper/Wallpaper'
 
 const Submission: React.FC = () => (
-  <div className="container">
-    
+  <div className="">
+    <Wallpaper />
+
+    <div className="submission-content">
+        <h1>Submission Form Coming Soon</h1>
+        <h3>Please check this week!</h3>
+    </div>
     <Footer /> 
   </div>
 );
