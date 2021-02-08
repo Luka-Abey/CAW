2203eb5559b914a31664e76082cce41ec23200ab
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-02-08 17:17:06 +0000

    got ids working

--- client/src/pages/SingleSubmission/SingleSubmission.tsx
+++ client/src/pages/SingleSubmission/SingleSubmission.tsx
@@ -6,23 +6,23 @@ import feedbackService from '../../services/feedbackService';
 import { RouteComponentProps } from 'react-router';
 
 interface MatchParams { 
-    submissionId: string;
+    id: string;
 }
 
 const SingleSubmission: React.FC<RouteComponentProps<MatchParams>> = (props) => {
 
     const [submission, setSubmission] = useState<ReceiveSubmissionType>();
 
-    const { submissionId } = props.match.params;
+    const submissionId = props.match.params.id;
 
+    console.log(submissionId)
+    
     const fService = new feedbackService();
 
     const fetchSubmission = () => {
         const sId = props.match.params;
-    
-        console.log(props.match.params.submissionId); 
-        console.log(props.match.params); 
-        // fService.getSubmissionById().then(response => setSubmission(response));
+        console.log(submissionId)
+        fService.getSubmissionById(submissionId).then(response => setSubmission(response));
     }
 
     useEffect( fetchSubmission,  []);
@@ -33,6 +33,7 @@ const SingleSubmission: React.FC<RouteComponentProps<MatchParams>> = (props) => 
     <div className="content-wrapper">
         <a className="btn btn-outline-success" href="/feedback">Back To Submissions</a>
         <h1> {submission?.title}</h1>
+        <p>{submission?.description}</p>
     </div>
 
     <Footer /> 
