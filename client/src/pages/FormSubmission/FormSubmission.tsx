f294eb8a554738f1ab370dd0a94d5735a5a62202
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-02-10 13:34:22 +0000

    appended form info and edited links tyle

--- client/src/pages/FormSubmission/FormSubmission.tsx
+++ client/src/pages/FormSubmission/FormSubmission.tsx
@@ -189,7 +189,9 @@ const Submission: React.FC = () => {
       <div className="pretext-wrapper"> 
         <h2 className="custom-heading">Submission Form</h2>
         <hr className="hr" />
-        <p className="p-text">Please submit your idea below. If you want some help, please check out our <a href="/submissions/example" className="link-to-example">example</a> idea for some guidance. Once your idea is submitted, it will be added to our database and displayed on the <a href="/submissions" className="link-to-example">submitted ideas</a> page. There, people will be able to add comments and questions.
+        <p className="p-text">Please submit your idea below. If you want some help, see our <a href="/submissions/example" className="link-to-example">example</a> idea for some guidance. Once your idea is submitted, it will be added to our database and displayed on the <a href="/submissions" className="link-to-example">submitted ideas</a> page. There, people will be able to add comments and questions.
+        <br/><br/>
+        If you would prefer to submit your idea in another format, such as by video or voice recording, please contact us via <a href="mailto:communityactionwaste@gmail.com?subject=Alternate Format Submission" className="link-to-example">email</a> and we can find a way that works for you.
         </p>
       </div>
       </div>
